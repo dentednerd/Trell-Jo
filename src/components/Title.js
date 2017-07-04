3d0b2f8a7f9d160096907a7523e3f98852a432c7
dentednerd <jo.imlay@gmail.com>
2017-07-04 11:37:05 +0100

    board title is editable

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,13 +5,41 @@ import Menu from './Menu';
 import './Title.css';
 
 class Title extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            editable: false,
+            pageTitle: 'title'
+        };
+        this.toggleEditable = this.toggleEditable.bind(this);
+        this.editTitle = this.editTitle.bind(this);
+    }
     render () {
         return (
             <div className="title-bar">
-                <button className="button">Title</button>
-                <Menu />
+                {
+                this.state.editable
+                ? <form onBlur={this.toggleEditable}>
+                    <input type="text" onChange={this.editTitle} className="titleInput" placeholder={this.state.pageTitle}></input>
+                </form>
+                : <button className="button" onClick={this.toggleEditable}>{this.state.pageTitle}</button>
+                
+                }
+            <Menu />
             </div>
         );
+    }
+
+    toggleEditable () {
+        this.setState ({
+            editable: !this.state.editable
+        });
+    }
+    editTitle (event) {
+        event.preventDefault();
+        this.setState({
+            pageTitle: event.target.value
+        });
     }
 }
 
